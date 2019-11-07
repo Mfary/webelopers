c01fd1c7f4dd7d9bcad185f9b36f6c1206543af0
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-11-07 17:19:30 +0330

    form div

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.forms import Form
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.core.mail import send_mail
 
 # Create your views here.
 from home.form import SignUpForm, SignInForm, FeedBack
@@ -56,10 +57,20 @@ def contact_us(request):
     if request.method == 'POST':
         form = FeedBack(request.POST)
         form.is_valid()
+        send_mail(
+            form.cleaned_data.get('title'),
+            form.cleaned_data.get('context') + form.cleaned_data.get('email'),
+            'django@django.com',
+            recipient_list=['clappleid@outlook.com'],
+            auth_user='mohammadsadeghkeshavarzi@yahoo.com',
+            auth_password='kSmS09193360118',
+            fail_silently=False,
+        )
         return redirect('/success')
     else:
         form = FeedBack()
     return render(request, 'feedback.html', {'form': form})
+
 
 def success(request):
     return render(request, 'success.html')
