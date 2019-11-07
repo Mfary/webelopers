209244660dce5b7de7bcfaa868b695f0a71e4a76
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-11-07 15:45:05 +0330

    Signup fields required

--- home/form.py
+++ home/form.py
@@ -5,16 +5,19 @@ from django.forms import Form
 
 
 class SignUpForm(UserCreationForm):
+    user_name = forms.CharField(max_length=20, required=True)
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(max_length=254, required=True)
     password1 = forms.CharField(
         label="password",
         widget=forms.PasswordInput,
+        required=True,
     )
     password2 = forms.CharField(
         label="password confirm",
         widget=forms.PasswordInput,
+        required=True,
     )
 
     class Meta:
