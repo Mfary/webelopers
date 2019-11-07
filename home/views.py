60bc67b1db3830c95b0ebe7b4d4a50236b677376
mfary <moha1379>
2019-11-07 15:14:00 +0330

    database

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -14,10 +14,12 @@ def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)
+            user.created_by = request.user
+            user.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+           # user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('/')
     else:
