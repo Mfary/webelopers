40873804a6f3a72bb3c8bf4b8e3a8bfc38bdaade
mfary <moha1379>
2019-11-07 14:46:27 +0330

    make signin form

--- home/form.py
+++ home/form.py
@@ -18,3 +18,8 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+
+
+class SignInForm(UserCreationForm):
+    username = forms.CharField(max_length=30 , required=True)
+    password = forms.CharField(max_length=30 , required=True)
