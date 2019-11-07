f9cc2cc3ece51a4c95ac6d1749effdaf7a0c66af
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-11-07 14:38:08 +0330

    Changed password fields

--- home/form.py
+++ home/form.py
@@ -7,9 +7,14 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(max_length=254, required=True)
-    password1 = forms.PasswordInput()
-    password2 = forms.PasswordInput()
-
+    password1 = forms.CharField(
+        label="password",
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label="password confirm",
+        widget=forms.PasswordInput,
+    )
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
